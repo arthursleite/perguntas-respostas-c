ff2cdd295d467f606e7fb6827d80e96ac2c2d469
Vithor Félix <vithorfjm@outlook.com>
2025-04-26 19:58:51 -0300

    Perguntas faceis e medias

--- programa.c
+++ programa.c
@@ -40,6 +40,10 @@ Questao obterQuestao(char dificuldade[15]);
 Questao obterPergunta(ListaQuestoes lista);
 void adicionarQuestao(ListaQuestoes *lista, Questao questao);
 ListaQuestoes criarQuestoesMuitoFaceis();
+ListaQuestoes criarQuestoesFaceis();
+ListaQuestoes criarQuestoesMedias();
+ListaQuestoes criarQuestoesDificeis();
+ListaQuestoes criarQuestoesMuitoDificeis();
 
 int main()
 {
@@ -210,14 +214,83 @@ ListaQuestoes criarQuestoesMuitoFaceis()
     adicionarQuestao(&lista, q2);
 
     Questao q3;
-    strcpy(q3.pergunta, "Qual símbolo é usado para comentar uma linha de código em C?");
+    strcpy(q3.pergunta, "Qual simbolo é usado para comentar uma linha de código em C?");
     strcpy(q3.opcaoCerta, "//");
     strcpy(q3.opcaoErrada1, "/*");
     strcpy(q3.opcaoErrada2, "#");
     strcpy(q3.opcaoErrada3, "--");
     adicionarQuestao(&lista, q3);
 
-    
-
     return lista;
+}
+
+ListaQuestoes criarQuestoesFaceis() {
+    ListaQuestoes lista;
+    lista.quantidadeQuestoes = 0;
+
+    Questao q1;
+    strcpy(q1.pergunta, "Como declarar um array de 5 numeros inteiros em C?");
+    strcpy(q1.opcaoCerta, "int array[5];");
+    strcpy(q1.opcaoErrada1, "array int[5];");
+    strcpy(q1.opcaoErrada2, "int[] array = new int[5];");
+    strcpy(q1.opcaoErrada3, "int array = {5};");
+    adicionarQuestao(&lista, q1);
+
+    Questao q2;
+    strcpy(q2.pergunta, "Qual estrutura de repeticao eh ideal quando o numero de iteracoes eh conhecido?");
+    strcpy(q2.opcaoCerta, "for");
+    strcpy(q2.opcaoErrada1, "while;");
+    strcpy(q2.opcaoErrada2, "do-while");
+    strcpy(q2.opcaoErrada3, "repeat");
+    adicionarQuestao(&lista, q2);
+
+    Questao q3;
+    strcpy(q3.pergunta, "Para que serve a diretiva #include?");
+    strcpy(q3.opcaoCerta, "Incluir bibliotecas externas.");
+    strcpy(q3.opcaoErrada1, "Declarar variaveis.");
+    strcpy(q3.opcaoErrada2, "Definir funcoes.");
+    strcpy(q3.opcaoErrada3, "Declarar variaveis constantes no programa.");
+    adicionarQuestao(&lista, q3);
+}
+
+ListaQuestoes criarQuestoesMedias() {
+    ListaQuestoes lista;
+    lista.quantidadeQuestoes = 0;
+
+    Questao q1;
+    strcpy(q1.pergunta, "O que o seguinte trecho de codigo imprime?\nchar *str = \"Hello\";\nprintf(\"%c\", *(str + 2));\n");
+    strcpy(q1.opcaoCerta, "l");
+    strcpy(q1.opcaoErrada1, "H");
+    strcpy(q1.opcaoErrada2, "e");
+    strcpy(q1.opcaoErrada3, "o");
+    adicionarQuestao(&lista, q1);
+
+    Questao q2;
+    strcpy(q2.pergunta, "O que a função sizeof retorna em C?");
+    strcpy(q2.opcaoCerta, "O numero de bytes ocupado por um tipo ou variavel.");
+    strcpy(q2.opcaoErrada1, "A quantidade de variáveis declaradas.");
+    strcpy(q2.opcaoErrada2, "A capacidade máxima de memória do computador.");
+    strcpy(q2.opcaoErrada3, "O valor armazenado em uma variável.");
+    adicionarQuestao(&lista, q2);
+
+    Questao q3;
+    strcpy(q3.pergunta, "Qual operador bitwise eh usado para inverter os bits de um numero?");
+    strcpy(q3.opcaoCerta, "~ (NOT)");
+    strcpy(q3.opcaoErrada1, "| (OR)");
+    strcpy(q3.opcaoErrada2, "& (AND)");
+    strcpy(q3.opcaoErrada3, "^ (XOR)");
+    adicionarQuestao(&lista, q3);
+}
+
+ListaQuestoes criarQuestoesDificeis() {
+    ListaQuestoes lista;
+    lista.quantidadeQuestoes = 0;
+
+    Questao q1;
+    strcpy(q1.pergunta, "Qual tipo de memoria eh usada para armazenar variaveis locais dentro de funcoes em C?");
+    strcpy(q1.opcaoCerta, "Pilha (stack).");
+    strcpy(q1.opcaoErrada1, "Heap.");
+    strcpy(q1.opcaoErrada2, "Memoria global.");
+    strcpy(q1.opcaoErrada3, "Memoria flash.");
+    adicionarQuestao(&lista, q1);
 }
